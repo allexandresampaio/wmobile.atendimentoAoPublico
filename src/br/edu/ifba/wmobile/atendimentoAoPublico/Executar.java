3f957e8cc303c925a8191cb2a9beca782c85ec96
Italo Miranda de Novais <italomirandadenovais@gmail.com>
2015-03-25 22:15:17 -0300

    Agora com o executar

---
+++ src/br/edu/ifba/wmobile/atendimentoAoPublico/Executar.java
@@ -0,0 +1,15 @@
+package br.edu.ifba.wmobile.atendimentoAoPublico;
+
+public class Executar {
+
+	public static void main(String[] args) {
+		
+		Atendente atendente = new AtdCalcados("João");
+		atendente.setProximo(new AtdVestFem("José"));
+		atendente.setProximo(new AtdVestMasc("Antônio"));
+		System.out.println(atendente.resolverDuvida(TipoDuvida.CALCADOS));
+		System.out.println(atendente.resolverDuvida(TipoDuvida.VESTFEM));
+		System.out.println(atendente.resolverDuvida(TipoDuvida.VESTMASC));
+	}
+
+}
